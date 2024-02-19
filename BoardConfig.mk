d61646e33d03f968dd45a6fb3be142c3d04a2e56
Jesse Hall <jessehall@google.com>
2024-02-19 01:00:17 +0000

    peridot: Use hwui vulkan backend
    The Vulkan backend is the intended production configuration and what we
    shipped gs101 with.
    
    Also checked remaining gpu configuration and minimized textual
    differences from the gs101 device.mk for easier comparison.
    
    Bug: 223406172
    Test: Launcher, YouTube video playback, Pixel Camera preview & shot
    Change-Id: I85e3c34ef424f7cef97bcfb6f5a10746fc0b9804
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,9 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 # Hardware
 BOARD_USES_QCOM_HARDWARE := true
 
+# Graphics
+TARGET_USES_VULKAN := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
