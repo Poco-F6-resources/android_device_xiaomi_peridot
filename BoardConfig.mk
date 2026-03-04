09bce378242e8a64cd017f15b4f63fbc4983fb6e
NotBlazeee28 <p.devansh@proton.me>
2026-03-04 11:25:08 +0000

    peridot: Bring back mispatializer, misoundfx and dolby stack
    This reverts commit d2ea4d399e6c6d7f600a32cb52f0c2484cd49460.
    
    * Bring dax configs to dt
    
    Change-Id: I9f8eaf4a2caba3af472110ffd769fcd461478761
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ TARGET_USES_VULKAN := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/device_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml
